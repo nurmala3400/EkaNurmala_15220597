a9b8e8f97ead183b25a412c7a3add443e14a601b
nurmala3400 <azmannurya@gmail.com>
2024-03-28 13:02:59 +0700

    Tugas1

--- application/controllers/tugas1.php
+++ application/controllers/tugas1.php
@@ -1,12 +1,12 @@
 <?php 
-class Contoh1 extends CI_Controller 
+class tugas1 extends CI_Controller 
 { 
     public function index() 
     { 
         echo "<h1>Perkenalkan</h1>"; 
-        echo"Nama saya Imam Nawawi 
-            Saya tingga di daerah Ciputat 
+        echo"Nama saya Eka Nurmala<br> 
+            Saya tinggal di Natuna<br>
             olah raga yang saya sukai adalah 
-            Bulutangkis"; 
+            Volly<br> Saya Seorang mahasiswa di Universitas Bina Sarana Informatika Semester 4"; 
     } 
 }
